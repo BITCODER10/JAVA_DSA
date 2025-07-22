9a0df4d897dead48b14f7d978e4b6440cee0e50c
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-22 22:45:16 +0530

    first commit

---
+++ src/main/java/com/logicalnil/Main.java
@@ -0,0 +1,13 @@
+package com.logicalnil;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println("This repository is created to practice Java Data Structures and Algorithm");
+        System.out.println();
+        System.out.println();
+    }
+}
