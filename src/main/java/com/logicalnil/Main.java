14f89a3ade4ef4fdbba3a8c9bdf1cdfe975dea94
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-27 20:18:28 +0530

    Comparison is completed , Arraylists are the best

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -1,13 +1,49 @@
 package com.logicalnil;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println("This repository is created to practice Java Data Structures and Algorithm");
-        System.out.println("Switch to different branches to check each data structure program ");
-        System.out.println();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+        for (int i = 0; i < 1_000_000; i++) {
+            linkedList.add(i);
+            arrayList.add(i);
+        }
+
+//        ---------------------- LinkedList -------------------------------
+        System.out.println(startTime = System.nanoTime());
+
+//        linkedList.add(500000, 5000);
+//        linkedList.remove((Integer) 500000);
+//        linkedList.getFirst();
+        linkedList.getLast();
+
+        System.out.println(endTime = System.nanoTime());
+        elapsedTime = endTime - startTime;
+        System.out.println("Total time required LinkedList: "+ (double)elapsedTime / 1_000_000_000);
+
+
+//        ---------------------- ArrayList -------------------------------
+        System.out.println(startTime = System.nanoTime());
+
+//        arrayList.add(500000, 5000);
+//        arrayList.remove((Integer) 500000);
+//        arrayList.getFirst();
+        arrayList.getLast();
+
+        System.out.println(endTime = System.nanoTime());
+        elapsedTime = endTime - startTime;
+        System.out.println("Total time required for ArrayList: "+ (double)elapsedTime / 1_000_000_000);
+
     }
 }
