45a666faeaace5b5d6925119b2a79bcf164a83d7
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-08-03 14:05:12 +0530

    Fixed  the mistake

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -8,19 +8,6 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("\nWhat is the length of your array : ");
-        int[] array =  new int[scanner.nextInt()];
-        Random random = new Random();
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(0,99);
-        }
-
-        System.out.println("Original array is : "+ Arrays.toString(array));
-        System.out.println();
-        BubbleSortClass bubbleSortObject = new BubbleSortClass(array);
-        System.out.println("\nSorted Array is : "+Arrays.toString(bubbleSortObject.sortArraylogic()));
-        scanner.close();
     }
 }
