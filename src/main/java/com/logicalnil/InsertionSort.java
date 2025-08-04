89d9ac9b25af7bf33ce3b003ed3b2dc65c2d2ee0
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-08-04 20:42:02 +0530

    InsertionSort Completed

--- src/main/java/com/logicalnil/InsertionSort.java
+++ src/main/java/com/logicalnil/InsertionSort.java
@@ -1,5 +1,31 @@
 package com.logicalnil;
 
-public class InsertionSort {
+import java.util.Arrays;
 
+public class InsertionSort {
+    private int[] array;
+    InsertionSort(){}
+    InsertionSort(int[] array){
+        this.array =array;
+    }
+    
+    public void runLogic(){
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i-1;
+
+            while (j>=0 && array[j]>temp){
+                array[j+1] = array[j];
+                j--;
+            }
+
+            array[j+1] = temp;
+            System.out.printf("\nIteration Number %d result is %s",i,this.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
+    }
 }
--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -1,7 +1,29 @@
 package com.logicalnil;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter size of the array : ");
+        int[] array = new int[scanner.nextInt()];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(99);
+        }
+
+        InsertionSort insertionSortObject = new InsertionSort(array);
+        System.out.print("\nAuto Generated Array : ");
+        System.out.println(insertionSortObject.toString());
+
+        insertionSortObject.runLogic();
+        System.out.println("\n\n\nFinal Result"+insertionSortObject.toString());
 
     }
 }
+
+//Remember Technic: For a While think about insertion
+//Its like finding the right place of value
